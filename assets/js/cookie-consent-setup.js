bcc77efec90ce21436147dbce8a80f96fa7bae77
xuhao1 <xuhao1@users.noreply.github.com>
2026-04-26 18:59:35 +0000

    Deploying to gh-pages from @ SOAR-Robotics-Lab/soar-robotics-lab.github.io@042dadbc6d7c4ee85f9093c2b351749c400c708c 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://xuhao1.me/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://www.soar-lab.org/#contact">contact us</a>.'
               }
             ]
           }
